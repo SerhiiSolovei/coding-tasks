cad092bd98d75b9ca0ed2d4c83d6ca94d376191f
Your Name <you@example.com>
2020-09-03 20:54:55 +0300

    Eighth lesson

---
+++ js/lesson-8.js
@@ -0,0 +1,42 @@
+/**
+ * Задача №1
+ *
+ * Написать функцию которая будет шифровать сообщение по Шифру Цезаря с заданным сдвигом.
+ * Справка: https://ru.wikipedia.org/wiki/%D0%A8%D0%B8%D1%84%D1%80_%D0%A6%D0%B5%D0%B7%D0%B0%D1%80%D1%8F
+ * Условие: Неалфавитные символы — знаки препинания, пробелы, цифры — не меняются.
+ */
+
+const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const normalAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+
+function transformStringToArray(string) {
+  const array = [];
+  for (let i = 0; i < string.length; i++) {
+    array.push(string[i]);
+  }
+
+  return array;
+}
+
+function caesarCipher(text, shift = 3) {
+  const alphabetArray = alphabet.split("");
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    const currentSymbol = text[i];
+    const currentSymbolIndex = alphabetArray.indexOf(
+      currentSymbol.toLowerCase()
+    );
+    const newSymbolIndex =
+      currentSymbolIndex < alphabetArray.length - shift
+        ? currentSymbolIndex + shift
+        : currentSymbolIndex + shift - alphabetArray.length;
+    const newSymbol =
+      currentSymbolIndex !== -1 ? alphabetArray[newSymbolIndex] : currentSymbol;
+    result += newSymbol;
+  }
+
+  return result;
+}
+
+const encryptedText = caesarCipher("Санёк ни за что не узнает что он ублюдок"); // Фгрин рл кг ъхс рз цкргзх ъхс ср ...
+console.log(encryptedText);
