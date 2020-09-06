b164d6f855ae77259149e31966a3ac1465b15bd5
Your Name <you@example.com>
2020-09-06 12:54:59 +0300

    EndsWith Examples

--- js/StringMethods/endsWith.js
+++ js/StringMethods/endsWith.js
@@ -5,7 +5,8 @@
  * Параметры: .endsWith(searchString, position?)
  *
  * searchString - Символы, искомые в конце данной строки.
- * position - Необязательный параметр. Искать внутри данной строки, как если бы эта строка была указанной длины; по умолчанию установлен в реальную длину строки, обрезается по диапазону, устанавливаемому длиной этой строки.
+ * position - Необязательный параметр. Искать внутри данной строки, как если бы эта строка была указанной длины;
+ * по умолчанию установлен в реальную длину строки, обрезается по диапазону, устанавливаемому длиной этой строки.
  *
  * Примеры:
  */
@@ -15,3 +16,10 @@ const str = "Быть или не быть, вот в чём вопрос.";
 str.endsWith("вопрос."); // true
 str.endsWith("быть"); // false
 str.endsWith("быть", 16); // true
+
+const newStr = "To be or not to be!";
+
+console.log(newStr.endsWith("be!")); // true
+console.log(newStr.endsWith("be")); // false
+console.log(newStr.endsWith("to be!")); // true
+console.log(newStr.endsWith("not", 12)); // true
