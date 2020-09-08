55b14394f8db1a421562c11104617010ef56dd00
Your Name <you@example.com>
2020-09-08 13:30:26 +0300

    ToPrecision Examples

--- js/NumberMethods/toPrecision.js
+++ js/NumberMethods/toPrecision.js
@@ -14,3 +14,12 @@ numObj.toPrecision(); // '5.123456'
 numObj.toPrecision(5); // '5.1235'
 numObj.toPrecision(2); // '5.1'
 numObj.toPrecision(1); // '5'
+
+const num = 53500;
+const floatNum = 654.4536;
+
+console.log(num.toPrecision(2)); // 5.4e+4
+
+console.log(floatNum.toPrecision(1)); // 7e+2
+console.log(floatNum.toPrecision(3)); // 654
+console.log(floatNum.toPrecision(4)); // 654.4
