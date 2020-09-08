76df1e86d42f34e389e0d990f831d615a72860dc
Your Name <you@example.com>
2020-09-08 14:12:59 +0300

    Updated ParseInt Examples

--- js/NumberMethods/parseInt.js
+++ js/NumberMethods/parseInt.js
@@ -18,3 +18,5 @@ Number.parseInt("10.33"); // returns 10
 Number.parseInt("10 20 30"); // returns 10
 Number.parseInt("10 years"); // returns 10
 Number.parseInt("years 10"); // returns NaN
+
+console.log(Number.parseInt("110", 2)); // 6
