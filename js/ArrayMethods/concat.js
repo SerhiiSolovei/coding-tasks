ea42838f1d06cfa8f190312fe6111eb43ea5735a
Your Name <you@example.com>
2020-09-10 15:27:18 +0300

    Updated concat Examples

--- js/ArrayMethods/concat.js
+++ js/ArrayMethods/concat.js
@@ -1,5 +1,5 @@
 /**
- * .concat() - возвращает новый массив, состоящий из массива, на котором он был вызван, соединённого с другими массивами и/или значениями, переданными в качестве аргументов.
+ * .concat() - возвращает НОВЫЙ!!! массив, состоящий из массива, на котором он был вызван, соединённого с другими массивами и/или значениями, переданными в качестве аргументов.
  *
  * Параметры: .concat(arr1, ..., arrN)
  *
@@ -12,3 +12,15 @@ const numeric = [1, 2, 3];
 const alphaNumeric = alpha.concat(numeric);
 
 console.log(alphaNumeric); // ['a', 'b', 'c', 1, 2, 3]
+
+const newArr = ["One", "Two", "Three"];
+const numArr = [1, 2, 3];
+const numArr2 = [4, 5, 6];
+
+console.log(newArr.concat(numArr)); // ["One", "Two", "Three", 1, 2, 3];
+console.log(newArr.concat(numArr).concat(numArr2)); // ["One", "Two", "Three", 1, 2, 3, 4, 5, 6];
+console.log(newArr.concat(numArr2, numArr)); // ["One", "Two", "Three", 4, 5, 6, 1, 2, 3];
+console.log(newArr.concat(1, numArr2, numArr)); // ["One", "Two", "Three", 1, 4, 5, 6, 1, 2, 3];
+console.log(newArr.concat(numArr, 7, 8, 9, numArr2)); // ["One", "Two", "Three", 1, 2, 3, 7, 8, 9, 4, 5, 6];
+console.log(newArr.concat(7, 8, 9)); // ["One", "Two", "Three", 7, 8, 9];
+console.log(newArr); // ["One", "Two", "Three"];
