d2ad2dfdaf9370cf8d4c85040d69a310d1cc685a
Artem Bodnia <deliriousraw@gmail.com>
2020-09-20 13:48:46 +0300

    added isArray method

---
+++ js/ArrayMethods/Array.isArray.js
@@ -0,0 +1,26 @@
+/**
+ * Array.isArray() - возвращает true, если значение является массивом и false, если он массивом не является.
+ *
+ * Параметры: Array.isArray(value)
+ *
+ * value - Значение, проверяемое на конечность.
+ *
+ * Примеры:
+ */
+
+// Все следующие вызовы вернут true
+Array.isArray([]);
+Array.isArray([1]);
+Array.isArray(new Array());
+// Малоизвестный факт: Array.prototype сам является массивом:
+Array.isArray(Array.prototype);
+
+// Все следующие вызовы вернут false
+Array.isArray();
+Array.isArray({});
+Array.isArray(null);
+Array.isArray(undefined);
+Array.isArray(17);
+Array.isArray("Array");
+Array.isArray(true);
+Array.isArray(false);
